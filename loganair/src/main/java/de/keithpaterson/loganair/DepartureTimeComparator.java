80eb97bba67570b94e2b04b85a1021fb94dac951
portree_kid <keith.paterson@gmx.de>
2018-01-22 07:42:15 +0100

    Fixed sorting
    Sort null records to start
    Sort by day

--- loganair/src/main/java/de/keithpaterson/loganair/DepartureTimeComparator.java
+++ loganair/src/main/java/de/keithpaterson/loganair/DepartureTimeComparator.java
@@ -8,15 +8,28 @@ final class DepartureTimeComparator implements Comparator<Flight> {
 
 	public DepartureTimeComparator() {
 	}
-	
+
 	public DepartureTimeComparator(boolean r) {
 		reverse = r;
 	}
 
 	public int compare(Flight o1, Flight o2) {
-		if (reverse)
+		if (reverse) {
+			if (o1.getDay() != o2.getDay())
+				return Integer.compare(o2.getDay(), o1.getDay());
+			if (o2.getDepartureTime() == null)
+				return -1;
+			if (o1.getDepartureTime() == null)
+				return 1;
 			return o2.getDepartureTime().compareTo(o1.getDepartureTime());
-		else
+		} else {
+			if (o1.getDay() != o2.getDay())
+				return Integer.compare(o1.getDay(), o2.getDay());
+			if (o1.getDepartureTime() == null)
+				return -1;
+			if (o2.getDepartureTime() == null)
+				return 1;
 			return o1.getDepartureTime().compareTo(o2.getDepartureTime());
+		}
 	}
 }
