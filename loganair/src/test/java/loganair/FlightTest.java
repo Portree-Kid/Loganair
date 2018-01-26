9eeac25a273991d2a36e7354b35b181323b62382
portree_kid <keith.paterson@gmx.de>
2018-01-26 10:06:53 +0100

    Correct tests

--- loganair/src/test/java/loganair/FlightTest.java
+++ loganair/src/test/java/loganair/FlightTest.java
@@ -18,15 +18,15 @@ public class FlightTest {
 	@Test
 	public void testSimple() throws ParseException {
 		Flight flight = new Flight("BE0012");
-		flight.setDeparture("08:00", "AA");
-		flight.setArrival("09:00", "BB");
+		flight.setDeparture("08:00 01-Jan", "AA");
+		flight.setArrival("09:00  01-Jan", "BB");
 		assertTrue(flight.getDepartureTime().before(flight.getArrivalTime()));
 	}
 
 	@Test
 	public void testLegging1() throws ParseException {
 		Flight flight = new Flight("BE0012");
-		flight.updateLeg("08:00", "AA", null, "BB");
+		flight.updateLeg("08:00 01-Jan", "AA", null, "BB");
 		flight.updateLeg(null, "AA", "09:00", "BB");
 		assertEquals(1,flight.getLegs().size());
 		assertTrue(flight.getDepartureTime().before(flight.getArrivalTime()));
@@ -35,9 +35,9 @@ public class FlightTest {
 	public void testLegging2() throws ParseException {
 		Flight flight = new Flight("BE0012");
 		flight.updateLeg(null, "AA", "09:00", "BB");
-		flight.updateLeg("08:00", "AA", null, "BB");
+		flight.updateLeg("08:00 01-Jan", "AA", null, "BB");
 		flight.updateLeg(null, "BB", "10:00", "CC");
-		flight.updateLeg("09:01", "BB", null, "CC");
+		flight.updateLeg("09:01      01-Jan", "BB", null, "CC");
 		assertEquals(2,flight.getLegs().size());
 		assertEquals("AA",flight.getFrom());
 		assertEquals("CC",flight.getTo());
@@ -47,10 +47,10 @@ public class FlightTest {
 	@Test
 	public void testLegging3() throws ParseException {
 		Flight flight = new Flight("BE0012");
-		flight.updateLeg(null, "BB", "10:00", "CC");
-		flight.updateLeg("09:01", "BB", null, "CC");
-		flight.updateLeg(null, "AA", "09:00", "BB");
-		flight.updateLeg("08:00", "AA", null, "BB");
+		flight.updateLeg(null, "BB", "10:00  01-Jan", "CC");
+		flight.updateLeg("09:01 01-Jan", "BB", null, "CC");
+		flight.updateLeg(null, "AA", "09:00 01-Jan", "BB");
+		flight.updateLeg("08:00 01-Jan", "AA", null, "BB");
 		assertEquals(2,flight.getLegs().size());
 		assertEquals("AA",flight.getFrom());
 		assertEquals("CC",flight.getTo());
@@ -60,10 +60,10 @@ public class FlightTest {
 	@Test
 	public void testLegging4() throws ParseException {
 		Flight flight = new Flight("BE0012");
-		flight.updateLeg("09:01", "BB", null, "CC");
-		flight.updateLeg("08:00", "AA", null, "BB");
-		flight.updateLeg(null, "BB", "10:00", "CC");
-		flight.updateLeg(null, "AA", "09:00", "BB");
+		flight.updateLeg("09:01 01-Jan", "BB", null, "CC");
+		flight.updateLeg("08:00 01-Jan", "AA", null, "BB");
+		flight.updateLeg(null, "BB", "10:00 01-Jan", "CC");
+		flight.updateLeg(null, "AA", "09:00 01-Jan", "BB");
 		assertEquals(2,flight.getLegs().size());
 		assertEquals("AA",flight.getFrom());
 		assertEquals("CC",flight.getTo());
